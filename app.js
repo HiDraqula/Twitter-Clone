cecc62e4b02dcb5b0fa8c9cd7b229658595a57f0
HiDraqula <aniketaz190@gmail.com>
2020-10-19 21:53:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const axios = require('axios');
+// const dotenv = require('dotenv');
+const Twitter = require('./api/helpers/twitter');
+const twitter = new Twitter();
+// dotenv.config();
+
+// console.log(process.env.TWITTER_API_TOKEN)
+
+app.use(express.static('client'))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("<h1> 👋 Hello World!! ✌😇🚀 </h1>")
+})
+app.get('/api/tweets', (req, res) => {
+    const query = req.query.q;
+    const resultType = req.query.result_type;
+    const maxId = req.query.max_id;
+    twitter.get(query, resultType, maxId).then((response) => {
+        res.status(200).send(response.data);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+})
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000/"));
